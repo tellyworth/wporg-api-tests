9ded8c09db4bcbe73b2a397c3a2e23890e8c2e07
tellyworth <alex@automattic.com>
2024-10-17 17:00:03 +1100

    Fix plugin update-check tests

--- tests/test-plugins-update-check.php
+++ tests/test-plugins-update-check.php
@@ -11,10 +11,11 @@ class TestPluginsUpdateCheck extends TestCase {
 		return [
 			[ 'https://api.wordpress.org/plugins/update-check/1.0/',
 			  [ 'plugins' => 'O:8:"stdClass":2:{s:7:"plugins";a:1:{s:57:"wordpress-auto-update-test/wordpress-auto-update-test.php";a:5:{s:4:"Name";s:25:"Example Autoupdate plugin";s:5:"Title";s:25:"Example Autoupdate plugin";s:11:"Description";s:3:"Bar";s:6:"Author";s:3:"Baz";s:7:"Version";s:3:"1.0";}}s:6:"active";a:1:{i:0;s:57:"wordpress-auto-update-test/wordpress-auto-update-test.php";}}' ],
-			  'text/plain; charset=utf-8'
+			  'text/plain; charset=utf-8',
+			  'a:0:{}',
 			],
 			[ 'https://api.wordpress.org/plugins/update-check/1.1/',
-			  '{
+			  [ 'plugins' =>  '{
     "plugins": {
         "wordpress-auto-update-test\/wordpress-auto-update-test.php": {
             "Name": "Example Autoupdate plugin",
@@ -27,11 +28,12 @@ class TestPluginsUpdateCheck extends TestCase {
     "active": [
         "wordpress-auto-update-test\/wordpress-auto-update-test.php"
     ]
-}',
-			  'application/json; charset=utf-8'
+}'],
+			  'application/json; charset=utf-8',
+			  '{"plugins":[],"translations":[]}'
 			],
 			[ 'https://api.wordpress.org/plugins/update-check/1.2/',
-			  '{
+			  [ 'plugins' => '{
     "plugins": {
         "wordpress-auto-update-test\/wordpress-auto-update-test.php": {
             "Name": "Example Autoupdate plugin",
@@ -44,8 +46,9 @@ class TestPluginsUpdateCheck extends TestCase {
     "active": [
         "wordpress-auto-update-test\/wordpress-auto-update-test.php"
     ]
-}',
-			  'application/json; charset=utf-8'
+}' ],
+			  'application/json; charset=utf-8',
+			  '{"plugins":[],"translations":[],"no_update":[]}'
 			],
 		];
 	}
@@ -53,21 +56,18 @@ class TestPluginsUpdateCheck extends TestCase {
 	/**
 	 * @dataProvider update_check_provider
 	 */
-	public function test_update_check( $url, $body, $expected_content_type ) {
+	public function test_update_check( $url, $body, $expected_content_type, $expected_body ) {
 		$args = [
-			'headers' => [
-				'Content-Type' => $expected_content_type,
-			],
 			'body' => $body,
 		];
-		var_dump( $url, $args );
-		$response = wp_remote_get( $url, $args );
-		var_dump( $response );
+
+		$response = wp_remote_post( $url, $args );
+
 		$this->assertEquals( 200, wp_remote_retrieve_response_code( $response ) );
 		$this->assertNotEquals( 'error', wp_remote_retrieve_body( $response ) );
-		var_dump( wp_remote_retrieve_header( $response, 'content-type' ) );
+
 		$this->assertEquals( $expected_content_type, wp_remote_retrieve_header( $response, 'content-type' ) );
-		var_dump( wp_remote_retrieve_body( $response ) );
+		$this->assertEquals( $expected_body, wp_remote_retrieve_body( $response ) );
 	}
 
 }
